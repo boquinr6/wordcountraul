1bd811e56f524fbd04951ea0693e8f8b86f7f12a
boquinr <kingra6@gmail.com>
2020-04-26 18:14:10 -0400

    Added Development Staging Prod Environments

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -13,4 +16,5 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
+    print(os.environ['APP_SETTINGS'])
     app.run()
